496026e2aa09e61e2ece0ecd05a41fcd2a928cf4
Ismael Cristal Jr <ismael.cristal@rocksolidinternet.com>
2017-08-14 21:42:48 +0800

    Adding toggle all functionality

--- todoapp/src/views/todoapplication/ListView.js
+++ todoapp/src/views/todoapplication/ListView.js
@@ -12,9 +12,31 @@ export default Marionette.View.extend({
         }
     },
 
+    ui: {
+        allCheckbox: 'label.btn'
+    },
+
+    events: {
+        'click @ui.allCheckbox': 'markAllItem'
+    },
+
     onRender: function() {
         this.showChildView('listBody', new ListBodyView({
             collection: this.collection
         }));
+    },
+
+    markAllItem: function() {
+        let completed = !this.ui.allCheckbox.hasClass('active');
+
+        this.collection.each(function(item) {
+            if (completed) {
+                item.markAsCompleted();
+            } else {
+                item.markAsIncomplete();
+            }
+
+            item.save();
+        });
     }
 });
