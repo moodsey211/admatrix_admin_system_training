7354d9613db798ed6f3794177eee36509ad8307e
Ismael Cristal Jr <ismael.cristal@rocksolidinternet.com>
2017-08-14 21:35:29 +0800

    Adding action to individual items

--- todoapp/src/views/todoapplication/ListItemView.js
+++ todoapp/src/views/todoapplication/ListItemView.js
@@ -3,5 +3,47 @@ import ListViewTemplate from './templates/ListViewTemplate.tpl';
 
 export default Marionette.View.extend({
     tagName: 'tr',
-    template: ListViewTemplate
+    template: ListViewTemplate,
+
+    ui: {
+        checkbox: 'label.btn',
+        deleteButton: 'a.btn span.glyphicon-remove'
+    },
+
+    events: {
+        'click @ui.checkbox': 'markItem',
+        'click @ui.deleteButton': 'removeItem'
+    },
+
+    modelEvents: {
+        change: 'render'
+    },
+
+    removeItem: function() {
+        this.model.destroy();
+    },
+
+    markItem: function() {
+        let completed = this.model.isCompleted();
+
+        if (completed) {
+            this.model.markAsIncomplete();
+        } else {
+            this.model.markAsCompleted();
+        }
+
+        this.model.save();
+    },
+
+    onRender: function() {
+        let completed = this.model.isCompleted();
+
+        if (completed) {
+            if (!this.$el.hasClass('cleared')) {
+                this.$el.addClass('cleared');
+            }
+        } else {
+            this.$el.removeClass('cleared');
+        }
+    }
 });
--- todoapp/src/views/todoapplication/templates/ListViewTemplate.tpl
+++ todoapp/src/views/todoapplication/templates/ListViewTemplate.tpl
@@ -1,12 +1,12 @@
 <td>
     <div class="btn-group" data-toggle="buttons">
-        <label class="btn btn-default btn-xs btn-checkbox">
+        <label class="btn btn-default btn-xs btn-checkbox<% if (completed) {%> active<%} %>">
             <input type="checkbox" autocomplete="off" />
             <span class="glyphicon glyphicon-ok"></span>
         </label>
     </div>
 </td>
-<td>Task One</td>
+<td><%= description %></td>
 <td align="right">
     <a href="#" class="btn btn-default btn-xs">
         <span class="glyphicon glyphicon-remove"></span>
